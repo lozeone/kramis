b0b88ccce1141b77a7a0bfd40ea0e4542d450717
loze <joe@ilgstudio.com>
2020-11-20 17:51:40 -0500

    getting glup to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,52 @@
-// DevCollab Starter Theme
+/**
+ * @file
+ * Gulpfile for compiling Sass.
+ */
+
 // https://css-tricks.com/gulp-for-beginners/
+const gulp = require('gulp');
+// Requires the gulp-sass plugin.
+const sass = require('gulp-sass');
+// Create sass sourcemaps.
+const sourcemaps = require('gulp-sourcemaps');
+// Delete generated files when needed.
+const del = require('del');
+// Run a list of tasks in order.
+// const runSequence = require('run-sequence');
+const runSequence = require('gulp4-run-sequence');
 
-var gulp = require('gulp');
-// Requires the gulp-sass plugin
-var sass = require('gulp-sass');
-// Create sass sourcemaps
-var sourcemaps = require('gulp-sourcemaps');
-// Delete generated files when needed
-var del = require('del');
-// Run a list of tasks in order
-var runSequence = require('run-sequence');
+// Used to set whether CSS format is compressed or expanded when compiled.
+const plumber = require('gulp-plumber');
+// Used to catch errors and continue build
+let style = 'compressed';
 
+gulp.task('sass', () => gulp.src('sass/**/*.scss')
+  .pipe(plumber(function(error){
+    console.log(error.message);
+    this.emit('end');
+  }))
+  .pipe(sourcemaps.init())
+  .pipe(sass({ outputStyle: style })) // Converts Sass to CSS with gulp-sass.
+  // .pipe(sourcemaps.write())
+  .pipe(gulp.dest('css')));
 
-gulp.task('sass', function(){
-  return gulp.src('sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('css'));
+gulp.task('clean:css', (cb) => {
+  del.sync('css/*')
+  cb();
 });
 
-gulp.task('clean:css', function(){
-  return del.sync('css/*');
-});
-
-gulp.task('watch', function(){
+gulp.task('watch', () => {
   gulp.watch('sass/**/*.scss', gulp.series('sass'));
 });
 
-// One time build process
-gulp.task('build', function() {
-  runSequence(
-    'clean:css', 'sass'
-  )
+// One time build process.
+gulp.task('build', (cb) => {
+  runSequence('clean:css', 'sass');
+  cb();
+});
+
+gulp.task('build-unminified', (cb) => {
+  style = 'expanded';
+  runSequence('build');
+  cb();
 });
